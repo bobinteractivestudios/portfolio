b8257efbd612226212f78ce40b5f4c6b85ca0fcc
bobinteractivestudios <bob@van-boekel.nl>
2026-09-21 12:48:14 +0200

    blacking out of personal info

--- components/ProgramPage.tsx
+++ components/ProgramPage.tsx
@@ -346,7 +346,9 @@ export default function ProgramPage() {
       </div>
 
       <footer className={styles.footer}>
-        <Link href="/">Bob van Boekel</Link>
+        <Link href="/" className={styles.footerName}>
+          Bob van Boekel
+        </Link>
         <div className={styles.footerContact}>
           <a href="tel:+31651775569">06 51 77 55 69</a>
           <a href="mailto:bob@van-boekel.nl">bob@van-boekel.nl</a>
